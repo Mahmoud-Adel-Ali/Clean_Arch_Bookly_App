d22581f9b2537523c8456bf70f07ee209ad17808
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-08-26 14:37:47 -0700

    fetch Feature Books from local storge

--- lib/Features/home/data/data_source/home_local_data_source.dart
+++ lib/Features/home/data/data_source/home_local_data_source.dart
@@ -1,4 +1,6 @@
 import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/constants.dart';
+import 'package:hive/hive.dart';
 
 abstract class HomeLocalDataSource {
   List<BookEntity> fetchFeatureBooks();
@@ -9,17 +11,18 @@ class HomeLocalDataSourceImpl implements HomeLocalDataSource {
   @override
   List<BookEntity> fetchFeatureBooks() {
     // Implementation for fetching featured books from local storage
-    // Example: return await _localDatabase.getFeaturedBooks();
-    return [];
+    var box = Hive.box<BookEntity>(kFeatureBooks);
+    List<BookEntity> books = box.values.toList();
+    return books;
   }
-  
+
   @override
   List<BookEntity> fetchNewestBooks() {
     // Implementation for fetching newest books from local storage
     // Example: return await _localDatabase.getNewestBooks();
     return [];
   }
-  
+
   // Add other methods for fetching other data sources if needed
   // For example: fetchTopRatedBooks(), fetchPopularAuthors(), fetchBestsellers()...
   // Implement the logic for fetching data from the respective data sources
@@ -29,4 +32,3 @@ class HomeLocalDataSourceImpl implements HomeLocalDataSource {
   // Remember to handle potential errors and edge cases when fetching data from the data sources
   // You can use try-catch blocks to handle errors and provide appropriate error handling
 }
-
--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // hive
+  await Hive.initFlutter();
   Hive.registerAdapter(BookEntityAdapter());
   await Hive.openBox(kFeatureBooks);
   // this step used to set device not rotated

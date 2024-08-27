0d839afaa838871ba466aaa570e750e9dc41f53c
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-08-27 13:44:14 -0700

    server failure  part  1

--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,1 +1,33 @@
-class Failure {}
+import 'package:dio/dio.dart';
+
+class Failure {
+  final String message;
+
+  Failure({required this.message});
+}
+
+class ServerFailure extends Failure {
+  ServerFailure({required super.message});
+
+  factory ServerFailure.fromDioError(DioException e) {
+    switch (e.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure(message: 'Connection time out with api server');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure(message: 'Send time out with api server');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure(message: 'Receive time out with api server');
+      case DioExceptionType.badCertificate:
+        return ServerFailure(message: 'badCertificate with api server');
+      case DioExceptionType.cancel:
+        return ServerFailure(message: 'Request to Api is canceld');
+      case DioExceptionType.connectionError:
+        return ServerFailure(message: 'No internet Connected');
+      case DioExceptionType.unknown:
+        return ServerFailure(
+            message: 'Oops : There was an error , please try agin');
+      case DioExceptionType.badResponse:
+    }
+  }
+
+}

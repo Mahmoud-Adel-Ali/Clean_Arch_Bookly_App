535ca9d7b87584764a74e12b79f6e63f63bc1c11
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-09-04 16:10:46 -0700

    refactor custom image

--- lib/Features/home/presentation/views/widgets/custom_book_item.dart
+++ lib/Features/home/presentation/views/widgets/custom_book_item.dart
@@ -8,12 +8,15 @@ class CustomBookImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return AspectRatio(
       aspectRatio: 2.6 / 4,
-      child: CachedNetworkImage(
-        imageUrl: image,
-        fit: BoxFit.fill,
-        placeholder: (context, url) =>
-            const Center(child: CircularProgressIndicator()),
-        errorWidget: (context, url, error) => const Icon(Icons.error),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16),
+        child: CachedNetworkImage(
+          imageUrl: image,
+          fit: BoxFit.fill,
+          placeholder: (context, url) =>
+              const Center(child: CircularProgressIndicator()),
+          errorWidget: (context, url, error) => const Icon(Icons.error),
+        ),
       ),
     );
   }

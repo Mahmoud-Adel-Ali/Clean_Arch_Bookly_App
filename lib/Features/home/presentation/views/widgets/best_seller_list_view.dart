1af89ded6e18896479980235e26168263c2a56a5
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-09-03 18:55:41 -0700

    display newest book data part one

--- lib/Features/home/presentation/views/widgets/best_seller_list_view.dart
+++ lib/Features/home/presentation/views/widgets/best_seller_list_view.dart
@@ -1,10 +1,11 @@
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
 import 'package:flutter/material.dart';
 
 import 'best_seller_list_view_item.dart';
 
 class BestSellerListView extends StatelessWidget {
-  const BestSellerListView({super.key});
-
+  const BestSellerListView({super.key, required this.books});
+  final List<BookEntity> books;
   @override
   Widget build(BuildContext context) {
     return ListView.builder(

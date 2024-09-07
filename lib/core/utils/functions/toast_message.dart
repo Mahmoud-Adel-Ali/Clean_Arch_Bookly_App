00f092636248af70102cc84764de6db179bb8131
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-09-07 14:46:51 -0700

    refactor flutter toast message

--- lib/core/utils/functions/toast_message.dart
+++ lib/core/utils/functions/toast_message.dart
@@ -1,13 +1,14 @@
+import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 toastMessage({required String msg}){
   return Fluttertoast.showToast(
         msg: msg,
         toastLength: Toast.LENGTH_SHORT,
-        gravity: ToastGravity.CENTER,
-        timeInSecForIosWeb: 1,
-        // backgroundColor: Colors.red,
-        // textColor: Colors.white,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 3,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
         fontSize: 16.0
     );
 }

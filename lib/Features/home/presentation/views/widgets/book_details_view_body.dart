dcb83dd7a798c3733de92bd8373ee9d9bcb33ddb
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-09-07 21:13:53 -0700

    book details part 2

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -17,19 +17,19 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(
+    return CustomScrollView(
       slivers: [
         SliverFillRemaining(
           hasScrollBody: false,
           child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 30),
+            padding: const EdgeInsets.symmetric(horizontal: 30),
             child: Column(
               children: [
-                CustomBookDetailsAppBar(),
-                BookDetailsSection(),
-                Expanded(child: SizedBox(height: 50)),
-                SimilarBooksSection(),
-                SizedBox(height: 40),
+                const CustomBookDetailsAppBar(),
+                BookDetailsSection(book: book),
+                const Expanded(child: SizedBox(height: 50)),
+                const SimilarBooksSection(),
+                const SizedBox(height: 40),
               ],
             ),
           ),
--- lib/Features/home/presentation/views/widgets/books_details_sectioni.dart
+++ lib/Features/home/presentation/views/widgets/books_details_sectioni.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../../core/utils/styles.dart';
@@ -6,8 +7,8 @@ import 'books_action.dart';
 import 'custom_book_item.dart';
 
 class BookDetailsSection extends StatelessWidget {
-  const BookDetailsSection({super.key});
-
+  const BookDetailsSection({super.key, required this.book});
+  final BookEntity book;
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
@@ -18,39 +19,34 @@ class BookDetailsSection extends StatelessWidget {
           padding: EdgeInsets.symmetric(
             horizontal: width * .2,
           ),
-          child: const CustomBookImage(image: ''),
+          child: CustomBookImage(image: book.image ?? ""),
         ),
-        const SizedBox(
-          height: 43,
-        ),
+        const SizedBox(height: 43),
         Text(
-          'The Jungle Book',
+          book.title,
+          textAlign: TextAlign.center,
           style: Styles.textStyle30.copyWith(
             fontWeight: FontWeight.bold,
           ),
         ),
-        const SizedBox(
-          height: 6,
-        ),
+        const SizedBox(height: 6),
         Opacity(
           opacity: .7,
           child: Text(
-            'Rudyard Kipling',
+            book.authorName ?? 'undefine auther',
+            textAlign: TextAlign.center,
             style: Styles.textStyle18.copyWith(
               fontStyle: FontStyle.italic,
               fontWeight: FontWeight.w500,
             ),
           ),
         ),
-        const SizedBox(
-          height: 18,
+        const SizedBox(height: 18),
+        BookRating(
+          mainAxisAlignment: MainAxisAlignment.center,
+          rate: book.rate ?? 0,
         ),
-        const BookRating(
-          mainAxisAlignment: MainAxisAlignment.center, rate: 0,
-        ),
-        const SizedBox(
-          height: 37,
-        ),
+        const SizedBox(height: 37),
         const BooksAction(),
       ],
     );
